0626e2f6e2cc4e1f2c0d727dcc0caa6a7495cd8c
SnoopyKim <nombu71@gmail.com>
2018-01-06 21:39:20 +0900

    Index test
    how php work?

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>WebTest1</title>
+  </head>
+  <body>
+    
+    <h1> input your information</h1>
+    <table border="1">
+      <tr>
+        <td>ID</td>
+        <td><input type="text" size="30" name="id"></td>
+      </tr>
+      <tr>
+        <td>Password</td>
+        <td><input type="pasword" size="30" name="pwd"></td>
+      </tr>
+    </table>
+    <input type="submit" value="submit">
+
+  </body>
+</html>
